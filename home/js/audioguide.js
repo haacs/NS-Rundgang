4beb5597fe4400fffd957d78324914423377e155
--local <lupuscogitans@users.noreply.github.com>
2016-04-14 22:01:02 +0200

    add audioguide

--- home/js/audioguide.js
+++ home/js/audioguide.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
-  var playbutton = $("#playbutton");
+  var playbutton = $(".playbutton");
   var guideelem = '<audio id="audiotest" controls src="' + playbutton.attr("data-src") + '" type="audio/mp3" autoplay></audio>';
   playbutton.click(function($){
     playbutton.replaceWith(guideelem);
