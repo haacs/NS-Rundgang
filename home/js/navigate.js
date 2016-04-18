604162578bd6e2e06aa0fd6c579c0ca41e64aeff
--local <lupuscogitans@users.noreply.github.com>
2016-04-18 20:21:49 +0200

    add javaScript map with geoloation

--- home/js/navigate.js
+++ home/js/navigate.js
@@ -1,20 +1,32 @@
 jQuery(document).ready(function($){
   $('.mapBtn').click(function($) {
-    var destination = this.dataset.destplaceid;
-    var origin = this.dataset.orgplaceid;
+    var destination = {
+      lat: Number(this.dataset.destlat),
+      lng: Number(this.dataset.destlng)
+    };
+    var origin = {
+      lat: Number(this.dataset.orglat),
+      lng: Number(this.dataset.orglng)
+    };
     var cookiecheck = jQuery('#cookiecheck');
     if ( cookiecheck.hasClass('glyphicon-check') && (getCookie('autoNav')!=='true')) {
       setCookie("autoNav", "true", 8064000);
     };
-    startNavigation(destination, origin);
+    startNavigation(origin, destination);
   });
 
   if (getCookie('autoNav')==true) {
     if (jQuery('#map').dataset.btn==="false") {
       var map = jQuery('#map');
-      var destination = map.dataset.destplaceid;
-      var origin = map.dataset.orgplaceid;
-      startNavigation(destination, origin);
+      var destination = {
+        lat: Number(map.dataset.destlat),
+        lng: Number(map.dataset.destlng)
+      };
+      var origin = {
+        lat: Number(map.dataset.orglat),
+        lng: Number(map.dataset.orglng)
+      };
+      startNavigation(origin, destination);
     } else {
       jQuery('#cookies').remove();
     };
@@ -26,8 +38,81 @@ jQuery(document).ready(function($){
 
 });
 
-function startNavigation (destination, origin) {
-  jQuery('#map').html('<iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyCPIQox75ruBBARUffohD7XMgcmSrOsJac&origin=place_id:' + origin + '&destination=place_id:' + destination + '&mode=walking" allowfullscreen></iframe>');
+function startNavigation (origin, destination) {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: origin,
+    zoom: 8
+  });
+  var posMarker = new google.maps.Marker({
+    position: origin,
+    title: 'Sie sind hier',
+  });
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('textdir'));
+  function setOrigin(pos){
+    origin = {lat: pos.coords.latitude,
+      lng: pos.coords.longitude};
+    calculateAndDisplayRoute(origin, destination, directionsService, directionsDisplay, posMarker);
+  };
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(setOrigin);
+  };
+  calculateAndDisplayRoute(origin, destination, directionsService, directionsDisplay, posMarker);
+}
+
+function calculateAndDisplayRoute(origin, destination, directionsService, directionsDisplay, posMarker) {
+  directionsService.route({
+    origin: origin,
+    destination: destination,
+    travelMode: google.maps.TravelMode.WALKING,
+    provideRouteAlternatives: false
+  }, function(result, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(result);
+      trackUser(result, directionsDisplay, posMarker);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+
+  function trackUser (result, directionsDisplay, posMarker) {
+    var trSteps = $('.adp-directions tr');
+    var mySteps = new Array();
+    var pos = null;
+    trSteps.map( function (index, elem){
+      mySteps[index] = {
+        finished: false,
+        elem: elem,
+        start: directionsDisplay.route[0].legs[elem.attr('data-leg-index')].steps[elem.attr('data-step-index')].start_location
+      };
+    });
+    var watchID = navigator.geolocation.watchPosition(function (position){
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      posMarker.setPosition(pos);
+      var matchInd = mySteps.forEach(function (myStep, index){
+        if( (myStep.start.lat + 0.01 ) > pos.lat && (myStep.start.lng - 0.01 ) < pos.lat ) {
+          return index;
+        };
+        return null;
+      });
+      if (matchInd) {
+        var finished = trSteps.slice(0, matchInd-1);
+        var active = trSteps.eq(matchInd);
+        finished.addClass('bg-info');
+        finished.removeClass('bg-primary')
+        active.addClass('bg-primary');
+        active.removeClass('bg-info');
+        if (matchInd === mySteps) {
+          navigator.geolocation.clearWatch(watchId);
+        };
+      };
+    });
+  };
 }
 
 function setCookie (identifier, value, Verfall) {
